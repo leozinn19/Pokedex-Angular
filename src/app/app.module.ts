365bf381db7f4f029bbff752221f71bbb49bd4c5
Leonardo Oliveira <leonardo.s.o.gc@gmail.com>
2023-02-14 16:47:37 -0300

    Criando Rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
@@ -9,8 +10,19 @@ import { PokemonService } from './services/pokemon.service';
 import { PokemonCategoryComponent } from './pokemon-category/pokemon-category.component';
 
 @NgModule({
-  declarations: [AppComponent, PokemonListComponent, PokemonCardComponent, PokemonCategoryComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    PokemonListComponent,
+    PokemonCardComponent,
+    PokemonCategoryComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'categories', component: PokemonCategoryComponent },
+    ]),
+  ],
   providers: [PokemonService],
   bootstrap: [AppComponent],
 })
